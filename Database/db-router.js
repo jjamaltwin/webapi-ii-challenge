dc6de6a6375bb80d6193fd0dc11850dbdd8e06f7
unknown <jjamal95@gmail.com>
2019-08-01 21:44:58 -0500

    got DELETE endpoint working

--- Database/db-router.js
+++ Database/db-router.js
@@ -99,5 +99,24 @@ router.get('/posts', async (req, res) => {
 }
   });
 
+  //  DELETE 
+
+  router.delete('/posts/:id', async (req, res) => {
+    try {
+      const count = await db.remove(req.params.id);
+      if (count > 0) {
+        res.status(200).json({ message: 'The post has been removed' });
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    } catch (error) {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error removing the post',
+      });
+    }
+  });
+
 
   module.exports = router ; 
--- Database/db.js
+++ Database/db.js
@@ -22,9 +22,11 @@ function findById(id) {
 }
 
 function insert(post) {
+  
   return db('posts')
     .insert(post)
     .then(ids => ({ id: ids[0] }));
+    
 }
 
 function update(id, post) {
